3c314f1ca310545f58cb3e6e321ca085038782de
Bohemian1506 <hiroshi151506@gmail.com>
2025-11-16 21:35:47 +0900

    ユーザーモデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Deviseモジュール
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+
+  # バリデーション
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :target_weight, numericality: { greater_than: 0 }, allow_nil: true
+  validates :current_weight, numericality: { greater_than: 0 }, allow_nil: true
 end
