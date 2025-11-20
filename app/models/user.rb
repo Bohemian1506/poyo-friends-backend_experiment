ff0ed17ad479b335d6b2444b5e9a7253575690ee
Pe-chan8 <hr.tp.892837@gmail.com>
2025-11-21 03:32:23 +0900

    Add validations to User model (#25)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Devise モジュール
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # バリデーション
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :target_weight, numericality: { greater_than: 0 }, allow_nil: true
+  validates :current_weight, numericality: { greater_than: 0 }, allow_nil: true
 end
